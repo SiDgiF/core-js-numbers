be8f89ec80f4e3e1f82c3c658a4cabf418257e98
Sergey <telmy@yandex.ru>
2023-11-05 01:07:42 +0300

    feat: added 10 solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -95,7 +95,7 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  */
 function getLinearEquationRoot(a, b) {
   // throw new Error('Not implemented');
-  const equation = b / a;
+  const equation = -b / a;
   return equation;
 }
 
@@ -117,8 +117,16 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  // throw new Error('Not implemented');
+  // скаляроное произведение векторов
+  const dotVector = x1 * x2 + y1 * y2;
+  // длины векторов
+  const lenght1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const lenght2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  // угол между двумя векторами
+  const angle = Math.acos(dotVector / (lenght1 * lenght2));
+  return angle;
 }
 
 /**
@@ -133,8 +141,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -148,8 +157,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  // throw new Error('Not implemented');
+  return +value;
 }
 
 /**
@@ -165,8 +175,11 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+
+function getParallelepipedDiagonal(a, b, c) {
+  // const lengthAB = Math.sqrt(a * a + b * b);
+  const diagonalLength = Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+  return diagonalLength;
 }
 
 /**
